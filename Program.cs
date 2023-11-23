2d99217aab63020d622dcfd07b72d89513fe7a5d
Pavel <grigorov_ps@mail.ru>
2023-11-23 19:34:53 +0300

    Создали программу, реающую поставленную задачу

--- Program.cs
+++ Program.cs
@@ -8,4 +8,35 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.Clear();
+Console.Write("Введите количество элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] s = new string[n];
+
+void inputArray(string[] array)
+{
+    int a =1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {a}-й элемент массива");
+        s[i] = Convert.ToString(Console.ReadLine()!);
+        a++;
+    } 
+}
+
+string[] sortArray(string[] str)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if(str[i].Length <= 3) Console.Write($"{str[i]}, ");
+    }
+    return str;
+}
+
+inputArray(s);
+Console.Write($"[{String.Join(", ", s)}]");
+
+Console.Write(" -> [");
+sortArray(s);
+Console.Write("]");
 
